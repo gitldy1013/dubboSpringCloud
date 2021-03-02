81706f9f87aadfadf28b6a3660c92426c30cdee8
刘东阳 <liudongyangds@chinamobile.com>
2021-03-02 10:56:25 +0800

    添加自定义路由转发规则

--- sc-gateway/src/main/java/org/cmcc/GatewayApp.java
+++ sc-gateway/src/main/java/org/cmcc/GatewayApp.java
@@ -3,6 +3,9 @@ package org.cmcc;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
 
 /**
  * @author cmcc
@@ -13,5 +16,15 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 public class GatewayApp {
     public static void main(String[] args) {
         SpringApplication.run(GatewayApp.class, args);
+    }
+
+    @Bean
+    public RouteLocator myRoutes(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route(p -> p
+                        .path("/task/query")
+                        .filters(f -> f.addRequestHeader("Hello", "World"))
+                        .uri("https://sms.liudongyang.top"))
+                .build();
     }
 }
