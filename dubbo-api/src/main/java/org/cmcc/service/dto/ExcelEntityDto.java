733ceab5718702a4af97dba561979ebc71d55274
刘东阳 <liudongyangds@chinamobile.com>
2021-03-17 16:24:34 +0800

    通用excel导出并提交到指定服务器功能提交

--- dubbo-api/src/main/java/org/cmcc/service/dto/ExcelEntityDto.java
+++ dubbo-api/src/main/java/org/cmcc/service/dto/ExcelEntityDto.java
@@ -1,13 +1,14 @@
-package org.cmcc.dto;
+package org.cmcc.service.dto;
 
 import lombok.Data;
 import lombok.ToString;
 
+import java.io.Serializable;
 import java.util.LinkedHashMap;
 
 @Data
 @ToString
-public class ExcelEntityDto {
+public class ExcelEntityDto implements Serializable {
     private String tableName;
 
     private LinkedHashMap<String, String> colms;
