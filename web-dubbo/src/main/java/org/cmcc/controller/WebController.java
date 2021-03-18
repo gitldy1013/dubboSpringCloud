3fb824bbd1e08d561cc47d7aef31a3ae253d761c
刘东阳 <liudongyangds@chinamobile.com>
2021-03-18 15:00:59 +0800

    bug修复

--- web-dubbo/src/main/java/org/cmcc/controller/WebController.java
+++ web-dubbo/src/main/java/org/cmcc/controller/WebController.java
@@ -6,6 +6,7 @@ import org.cmcc.service.ExcelExportService;
 import org.cmcc.service.dto.ExcelEntityDto;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -25,6 +26,7 @@ import java.util.Map;
  * @date 2020/6/26
  */
 @Controller
+@CrossOrigin(origins = "*")
 public class WebController {
     @Autowired
     private DubboService dubboService;
