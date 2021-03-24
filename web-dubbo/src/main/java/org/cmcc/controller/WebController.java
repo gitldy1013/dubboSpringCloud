0ecdcfdc31415f749858ebd276883342821d5f8e
刘东阳 <liudongyangds@chinamobile.com>
2021-03-24 13:31:44 +0800

    界面布局调整 集成服务 菜单分级

--- web-dubbo/src/main/java/org/cmcc/controller/WebController.java
+++ web-dubbo/src/main/java/org/cmcc/controller/WebController.java
@@ -86,9 +86,14 @@ public class WebController {
         return excelExportService.tableList(tableName);
     }
 
-    @GetMapping({"/","index",""})
+    @GetMapping({"/", "index", ""})
     public String index() {
         return "index";
+    }
+
+    @GetMapping({"/excel/home"})
+    public String home() {
+        return "home";
     }
 
     @GetMapping("/excel/index")
